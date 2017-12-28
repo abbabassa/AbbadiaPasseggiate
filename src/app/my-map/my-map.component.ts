1aaac4c27e2f2fae21dec7d238e5bf062a0ab361
Paolo <paolo@localhost.localdomain>
2017-12-28 12:02:37 +0100

    prova

--- src/app/my-map/my-map.component.ts
+++ src/app/my-map/my-map.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {API_KEY_BING} from './api-key-bing'
 // import {layer, source, Attribution, View, Map} from 'openlayers';
 import 'ol/ol.css';
 import Map from 'ol/map';
@@ -38,7 +39,7 @@ export class MyMapComponent implements OnInit {
         new TileLayer({
           source: new BingSource({
            // key: 'Ak-dzM4wZjSqTlzveKz5u0d4IQ4bRzVI309GxmkgSVr1ewS6iPSrOvOKhA-CJlm3',
-            key: 'Ai9-269FPOE2hv5jRs1OksKKe-XaEyr_TC_61-gmnVMyIz3kzp5YvL50vjj1KiiS',
+            key: API_KEY_BING.key,
             // use maxZoom 19 to see stretched tiles instead of the BingMaps
             // "no photos at this zoom level" tiles
             // maxZoom: 19
